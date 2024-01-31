184a1d751ed63a2a613d2e3b726e163e8be50322
EricLiao <ericxiang851019@gmail.com>
2024-01-31 19:21:34 +0800

    first commit

---
+++ prjAjaxHomework/Controllers/ApiController.cs
@@ -0,0 +1,38 @@
+﻿using AjaxDemoV2.Models.DTO;
+using Microsoft.AspNetCore.Mvc;
+using prjAjaxHomework.Models;
+using System.Text;
+
+namespace prjAjaxHomework.Controllers
+{
+    public class ApiController : Controller
+    {
+        private MyDBContext _dbContext;
+        public ApiController(MyDBContext dbContext) 
+        { 
+            _dbContext = dbContext;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CheckAccount(UserInfoDTO userDTO)
+        {
+            if (_dbContext.Members.FirstOrDefault(d => d.Name == userDTO.Name) != null)
+            {
+                return Content("帳號已存在", "text/plain", Encoding.UTF8);
+            }
+            return Content("帳號可使用", "text/plain", Encoding.UTF8);
+        }
+
+        [HttpPost]
+        public IActionResult Message(UserInfoDTO userDTO)
+        {
+
+            // return Content($"{userDTO.Name}, {userDTO.Email}, {userDTO.Age} 123", "text/plain", Encoding.UTF8);
+            return Content($"嗨{userDTO.Name}， 年齡: {userDTO.Age}， 電子郵件: {userDTO.Email}", "text/plain", Encoding.UTF8);
+        }
+    }
+}
---
+++ prjAjaxHomework/Models/DTO/UserInfoDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace AjaxDemoV2.Models.DTO
+{
+    public class UserInfoDTO
+    {
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public int? Age { get; set; }
+    }
+}
